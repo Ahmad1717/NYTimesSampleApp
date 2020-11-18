37e78b3565dac1503761a1c24f18e642dc86e58b
jabri <ahmad.jabri@nagarro.com>
2020-11-18 17:28:37 +0400

    add base, welcome and listing controller and add it to storyboard

---
+++ NYTimesSampleApp/Controllers/Base/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  NYTimesSampleApp
+//
+//  Created by Ahmad Jabri on 18/11/2020.
+//  Copyright © 2020 Ahmad Jabri. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.barTintColor = UIColor.navigationColor
+    }
+}
