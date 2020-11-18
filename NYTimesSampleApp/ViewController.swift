fc3b97e33a0c170635814a7579f7d1622e84b10d
jabri <ahmad.jabri@nagarro.com>
2020-11-18 11:02:31 +0400

    Initial Commit

---
+++ NYTimesSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTimesSampleApp
+//
+//  Created by Ahmad Jabri on 18/11/2020.
+//  Copyright © 2020 Ahmad Jabri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
